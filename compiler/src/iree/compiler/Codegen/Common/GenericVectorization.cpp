d72d8a8ce344e69c701f43a8ece83360b07fdc14
Benjamin Maxwell <benjamin.maxwell@arm.com>
2024-07-03 10:05:42 +0000

    WIP

--- compiler/src/iree/compiler/Codegen/Common/GenericVectorization.cpp
+++ compiler/src/iree/compiler/Codegen/Common/GenericVectorization.cpp
@@ -27,11 +27,37 @@ namespace mlir::iree_compiler {
 
 namespace {
 
+using DimBoundSize = vector::ConstantOrScalableBound::BoundSize;
+
 struct VectorizationTileSizes {
   SmallVector<int64_t> destShape;
   SmallVector<int64_t> vectorSizes;
   SmallVector<bool> vectorScalableFlags;
 };
+
+/// Computes the upper bound of `operandDim` for the ShapedType `operand`. If
+/// the optional `vscaleRange` is provided then the computed bound can be a
+/// scalable quantity.
+static FailureOr<DimBoundSize>
+computeDimUpperBound(Value operand, unsigned operandDim,
+                     std::optional<VscaleRange> vscaleRange) {
+  if (!vscaleRange.has_value()) {
+    auto maybeDimBoundSize = ValueBoundsConstraintSet::computeConstantBound(
+        presburger::BoundType::UB, {operand, operandDim},
+        /*stopCondition=*/nullptr, /*closedUB=*/true);
+    if (succeeded(maybeDimBoundSize))
+      return DimBoundSize{.baseSize = *maybeDimBoundSize, .scalable = false};
+    return failure();
+  }
+  auto maybeDimBound =
+      vector::ScalableValueBoundsConstraintSet::computeScalableBound(
+          operand, operandDim,
+          /*vscaleMin=*/vscaleRange->min,
+          /*vscaleMax=*/vscaleRange->max, presburger::BoundType::UB);
+  if (succeeded(maybeDimBound))
+    return maybeDimBound->getSize();
+  return failure();
+}
 
 /// Returns a VectorizationTileSizes which contains the inferred bounded result
 /// shape and vector input sizes. This is useful to infer the sizes from a
@@ -43,13 +69,25 @@ static std::optional<VectorizationTileSizes> inferSizesFromIR(Value val);
 /// Returns std::nullopt if vector sizes can't be inferred.
 static std::optional<VectorizationTileSizes>
 inferSizesFromIR(linalg::LinalgOp linalgOp, std::optional<OpResult> opResult) {
-  LLVM_DEBUG(VEC_DBGS() << "Inferring sizes for:\n"
-                        << linalgOp << " with OpResult.resultNumber="
-                        << opResult->getResultNumber() << "\n");
+  LLVM_DEBUG({
+    VEC_DBGS() << "Inferring sizes for:\n" << linalgOp;
+    if (opResult) {
+      VEC_DBGS() << " with OpResult.resultNumber="
+                 << opResult->getResultNumber();
+    }
+    VEC_DBGS() << '\n';
+  });
+
+  std::optional<VscaleRange> vscaleRange;
+  if (!opResult) {
+    // Note: Inferring scalable sizes is not supported is `opResult` is set
+    // (which is used to compute sizes for tensor.pack/unpack).
+    auto targetAttr = IREE::HAL::ExecutableTargetAttr::lookup(linalgOp);
+    vscaleRange = getDefaultVscaleRange(targetAttr);
+  }
 
   VectorizationTileSizes result;
   unsigned numDims = linalgOp.getNumLoops();
-
   for (int dim = 0; dim < numDims; ++dim) {
     // Map dimension `dim` to an operand dimension that we will use to
     // traverse the U-D chain to get `dim` vector size information.
@@ -75,16 +113,11 @@ inferSizesFromIR(linalg::LinalgOp linalgOp, std::optional<OpResult> opResult) {
     }
 
     // Use ValueBounds analysis to infer `dim` size upper bound.
-    FailureOr<vector::ConstantOrScalableBound> maybeDimBound;
+    FailureOr<DimBoundSize> maybeDimBound;
     for (auto operandDimPair : operandDimPairs) {
       Value operand = operandDimPair.first;
       unsigned operandDim = operandDimPair.second;
-      maybeDimBound =
-          vector::ScalableValueBoundsConstraintSet::computeScalableBound(
-              operand, operandDim,
-              /*vscaleMin=*/1,
-              /*vscaleMax=*/16, presburger::BoundType::UB);
-
+      maybeDimBound = computeDimUpperBound(operand, operandDim, vscaleRange);
       if (succeeded(maybeDimBound)) {
         break;
       }
@@ -94,13 +127,8 @@ inferSizesFromIR(linalg::LinalgOp linalgOp, std::optional<OpResult> opResult) {
       return std::nullopt;
     }
 
-    auto maybeDimSize = maybeDimBound->getSize();
-    if (failed(maybeDimSize)) {
-      return std::nullopt;
-    }
-
-    dimSize = maybeDimSize->baseSize;
-    dimScalable = maybeDimSize->scalable;
+    dimSize = maybeDimBound->baseSize;
+    dimScalable = maybeDimBound->scalable;
     result.vectorSizes.push_back(dimSize);
     result.vectorScalableFlags.push_back(dimScalable);
 
@@ -110,9 +138,8 @@ inferSizesFromIR(linalg::LinalgOp linalgOp, std::optional<OpResult> opResult) {
   }
 
   if (opResult) {
-    if (llvm::is_contained(result.vectorScalableFlags, true)) {
-      return std::nullopt;
-    }
+    assert(!llvm::is_contained(result.vectorScalableFlags, true) &&
+           "inferring scalable bounds with `opResult` not supported!");
     result.destShape = linalgOp.getIndexingMapMatchingResult(opResult.value())
                            .compose(result.vectorSizes);
   }
